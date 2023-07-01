c567f3b92b6fe5abaed065e57cbc91d6db2a0cff
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-01 16:12:23 -0400

    Updated Memory Functions + Added Integer userInput

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -22,6 +22,12 @@ public class Console {
         return userInput;
     }
 
+    public static Integer getDoubleInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        println(prompt);
+        int userInput = scanner.nextInt();
+        return userInput;
+
     public static Double getDoubleInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);

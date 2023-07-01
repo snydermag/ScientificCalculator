40ae3d954257c484a41cb1ebb66a710dc490ecb2
maggie <snydermag@gmail.com>
2023-07-01 14:29:25 -0400

    Added Switch cases

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -6,6 +6,8 @@ package com.zipcodewilmington.scientificcalculator;
 public class MainApplication {
     public static void main(String[] args) {
         boolean inMenu = true;
+        double currVal = 0;
+        double storedVal = 0;
 
 //        Console.println("Welcome to my calculator!");
 //        String s = Console.getStringInput("Enter a string");
@@ -18,6 +20,41 @@ public class MainApplication {
         while (inMenu) {
             printMainMenu();
 
+            String menuSelection = Console.getStringInput("Press Key to select Menu option");
+
+            switch (menuSelection) {
+                // User selects Clear - Will clear the current value and reset it to 0
+                case "C":
+                    break;
+
+                // User selects Inverse - Will negate the current value
+                case "I":
+                    break;
+
+                // User selects M+ - Will store the value for later use
+                case "M":
+                    break;
+                case "A":
+                    break;
+                case "E":
+                    break;
+                case "T":
+                    break;
+                case "L":
+                    break;
+                case "S":
+                    break;
+                case "Q":
+                    break;
+                default:
+                    System.out.println("Invalid input");
+                    break;
+            }
+
+
+
+
+
             inMenu = false;
 
         }
@@ -26,9 +63,8 @@ public class MainApplication {
 
     }
     public static void printMainMenu() {
-        System.out.format("Main Menu");
-        System.out.format("*Press key to select Menu Option*");
-        System.out.format("%s %s %s","C - Clear", "I - +/-", "M - M+");
+        System.out.format("Main Menu\n");
+        System.out.format("%s %s %s","C - Clear", "I - +/-", "M - M+\n");
         System.out.format("______");
         System.out.format("A - Arithmetic");
         System.out.format("E - Exponential");

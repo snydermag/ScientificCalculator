88be376ef4cf70f759234f84730b8a75cc7ddad0
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-01 12:41:17 -0400

    Finished Log

--- src/main/java/com/zipcodewilmington/scientificcalculator/Logarithm.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Logarithm.java
@@ -1,4 +1,21 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class Logarithm {
+    public static void main(String[] args) {
+    }
+    public static double getLN (double inputA) {
+
+        double result = Math.log(inputA);
+        return result;
+    }
+
+    public static double getLog10 (double inputA) {
+
+        double result = Math.log10(inputA);
+        return result;
+    }
+
+
+
+
 }

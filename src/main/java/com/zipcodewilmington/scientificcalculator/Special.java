82a545bca425afffe136c2bfee7634c66c56c1e0
anudeep-chennuri <anudeep.chennuri@gmail.com>
2023-07-01 11:31:09 -0400

    Trig finished

--- src/main/java/com/zipcodewilmington/scientificcalculator/Special.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Special.java
@@ -1,4 +1,24 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class Special extends Function {
+
+    public static void main(String[] args) {
+    }
+
+
+    public static double getAbsValue(double inputA) {
+        double result;
+        result = Math.abs(inputA);
+        return result;
+    }
+
+    public static double pythagoreanTheorem (double inputA, double inputB) {
+        double result;
+        result = Math.get;
+        return result;
+
+
+
+
+
 }

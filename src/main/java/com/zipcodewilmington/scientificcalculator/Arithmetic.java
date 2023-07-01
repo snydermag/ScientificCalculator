0929cbb1ca15e0b202f946d7fabd9430c5985fe6
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-01 11:33:59 -0400

    finished Arithmetic

--- src/main/java/com/zipcodewilmington/scientificcalculator/Arithmetic.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Arithmetic.java
@@ -1,6 +1,41 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class Arithmetic extends Function {
+    public static void main(String[] args) {
+
+    }
+    public static double getSum (double inputA , double inputB) {
+        double result = 0;
+
+        result = inputA + inputB;
+
+        return result;
+    }
+
+    public static double getSubtract (double inputA , double inputB) {
+
+        double result = 0;
+
+        result = inputA - inputB;
+
+        return result;
+    }
+
+    public  static double getMultiply (double inputA , double inputB) {
+        double result = 0 ;
+
+        result = inputA * inputB;
+
+        return result;
+    }
+
+    public  static double getDivide (double inputA , double inputB) {
+        double result = 0 ;
+
+        result = inputA / inputB;
+
+        return  result;
+    }
 
 
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/Function.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Function.java
@@ -5,11 +5,15 @@ public class Function {
 
     public static void main(String[] args) {
 
-
-
     }
+    public static double getSubtract (double inputA , double inputB) {
 
+        double result = 0;
 
+        result = inputA - inputB;
+
+        return result;
+    }
 
 
 }

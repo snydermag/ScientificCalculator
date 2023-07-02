383828ae72f05ec39c73f3b67328ad7b55649447
maggie <snydermag@gmail.com>
2023-07-02 17:12:15 -0400

    Fixed Radian/Degree thing

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -233,15 +233,16 @@ public class MainApplication {
                             inputA = Console.getDoubleInput("Input a number:");
                             currVal = Function.getInverseTan(inputA);
                             break;
-                        // User selects Degree to Radians - Will convert the number provided by User
-                        case "G" :
-                            inputA = Console.getDoubleInput("Input a number:");
-                            currVal = Function.degToRad(inputA);
+                        // User selects Degrees/Radians - Will convert the number provided by User
+                        case "R" :
+                            if (isRadians) {
+                                currVal = Function.radToDeg(currVal);
+                            }
+                            else {
+                                currVal = Function.degToRad(currVal);
+                            }
                             break;
-                        // User selects Radians to Degrees - Will convert the number provided by User
-                        case "H":
-                            inputA = Console.getDoubleInput("Input a number:");
-                            currVal = Function.radToDeg(inputA);
+
                         // User selects Quit - Will return to Main Menu
                         case "Q":
                             break;
